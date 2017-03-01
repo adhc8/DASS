44322004c09574073ef9070cb1797315f0271d9d
Richard <rbeddoes@shaw.ca>
2017-03-01 12:23:14 -0700

    made a few changes to Use_Sensors

--- Simulation/Use_Sensors.py
+++ Simulation/Use_Sensors.py
@@ -26,19 +26,29 @@ class engage_target:
                 self.target_info.append(float(coord))
             except:
                 self.target_info.append(0)
+    def target_dist(self):
+        if self.target_info[2] != 0:
+            target_distance = self.target_info[2]
+        elif self.target_info[0] != 0:
+            width = self.target_info[0] # in cm
+            target_distance = width*self.target_info[3]/const_cm_pix
+        else:
+            guess_width = 20 # in cm
+            target_distance = guess_width*self.target_info[3]/const_cm_pix
+        return target_distance
 
     def calc_x_velocity(self):
         if self.target_info[0] == 0:
             x_vel = 0
         else:
-            x_vel = round((self.target_info[0]-x_centre)/2,3)*100 # m/s to one decimal
+            x_vel = (round((self.target_info[0]-x_centre)/2,3)*100)/const_cm_pix # m/s to one decimal
         return x_vel
 
     def calc_y_velocity(self):
         if self.target_info[1] == 0:
             y_vel = 0
         else:
-            y_vel = round((self.target_info[1]-y_centre)/2,3)*100 #m/s to one decimal
+            y_vel = (round((self.target_info[1]-y_centre)/2,3)*100)/const_cm_pix #m/s to one decimal
         return y_vel
 
     def get_angle_to_target(self):
@@ -48,18 +58,9 @@ class engage_target:
             target_angle = math.atan((self.target_info[0]-x_centre)/self.z)
         return target_angle
 
-    def guess_dist(self):
-        if self.target_info[3] == 0:
-            guess_distance = 0
-        else:
-            guess_width = 20 # in cm
-            guess_distance = guess_width*self.target_info[3]/const_cm_pix
-        return guess_distance
-
     def target_Area(self):
         if self.target_info[3]==0:
             target_A = 0
         else:
             target_A = self.target_info[3]*self.target_info[4]
-
         return target_A
