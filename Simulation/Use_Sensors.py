ec8059877640c5dfc4d7175ed96176e760ec09aa
Richard <rbeddoes@shaw.ca>
2017-03-01 13:41:00 -0700

    changed a couple of calcs in Simulation

--- Simulation/Use_Sensors.py
+++ Simulation/Use_Sensors.py
@@ -4,7 +4,7 @@ import serial
 # self.target_info = x,y,z,w,h
 class engage_target:
 
-    const_cm_pix = 0.44963
+    const_cm_pix = 0.44963 # this mutiplied by distance gives cm/pix
     x_centre = 160
     y_centre = 100
 
@@ -26,29 +26,27 @@ class engage_target:
                 self.target_info.append(float(coord))
             except:
                 self.target_info.append(0)
+
     def target_dist(self):
         if self.target_info[2] != 0:
             target_distance = self.target_info[2]
-        elif self.target_info[0] != 0:
-            width = self.target_info[0] # in cm
-            target_distance = width*self.target_info[3]/const_cm_pix
         else:
             guess_width = 20 # in cm
-            target_distance = guess_width*self.target_info[3]/const_cm_pix
+            target_distance = guess_width/(elf.target_info[3]*const_cm_pix)
         return target_distance
 
     def calc_x_velocity(self):
         if self.target_info[0] == 0:
             x_vel = 0
         else:
-            x_vel = (round((self.target_info[0]-x_centre)/2,3)*100)/const_cm_pix # m/s to one decimal
+            x_vel = (round((self.target_info[0]-x_centre)/2,3)*100)/const_cm_pix*target_distance # m/s to one decimal
         return x_vel
 
     def calc_y_velocity(self):
         if self.target_info[1] == 0:
             y_vel = 0
         else:
-            y_vel = (round((self.target_info[1]-y_centre)/2,3)*100)/const_cm_pix #m/s to one decimal
+            y_vel = (round((self.target_info[1]-y_centre)/2,3)*100)/const_cm_pix*target_distance #m/s to one decimal
         return y_vel
 
     def get_angle_to_target(self):
